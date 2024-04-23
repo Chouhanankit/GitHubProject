d684e1b743a18a1bdf5c997e226267988320c46a
Ankit Chouhan <99067327+Chouhanankit@users.noreply.github.com>
2024-04-23 17:36:33 +0530

    3 rd comm

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { getAllUsers } from '../features/auth/authSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import DetailsPage from '../components/DetailsPage'
-import { CiSearch } from "react-icons/ci";
 
 
 const Home = () => {
